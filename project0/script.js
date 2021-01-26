869fa1f5a009b0c5b5ea72a0c1cc18a6fcf8d7c5
clintonhays <clintonhays84@gmail.com>
2021-01-26 16:21:05 +0900

    add fucntionality to newTodo function

--- project0/script.js
+++ project0/script.js
@@ -8,7 +8,26 @@ const classNames = {
 const list = document.getElementById("todo-list");
 const itemCountSpan = document.getElementById("item-count");
 const uncheckedCountSpan = document.getElementById("unchecked-count");
+const checkboxes = document.querySelectorAll(".todo-checkbox");
+
+itemCountSpan.innerHTML = 0;
+let itemCount = 0;
+
+uncheckedCountSpan.innerHTML = 0;
+let uncheckedCount = 0;
 
 function newTodo() {
-  alert("New TODO button clicked!");
+  const todo = prompt("What would you like...todo?");
+  const newLi = document.createElement("li");
+
+  newLi.innerHTML = `
+    <li class=${classNames.TODO_ITEM}>
+      <p class=${classNames.TODO_TEXT}>${todo}</p>
+      <input type="checkbox" class=${classNames.TODO_CHECKBOX}>
+    <li>
+  `;
+
+  list.appendChild(newLi);
+  itemCount++;
+  itemCountSpan.innerHTML = itemCount;
 }
